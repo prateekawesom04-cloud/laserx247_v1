181eee87708de3788a8b8ef8828777fa0c19b864
juhi awesome007 <juhiawesome03@gmail.com>
2025-08-25 18:32:19 +0530

    change in header design

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -21,14 +21,14 @@
   </div>
   <div class="offcanvas-body p-0 bg-light">
     <div class="list-group list-group-flush">
+      <div class="menu-item"><i class="fas fa-user me-2"></i> Profile</div>
       <div class="menu-item"><i class="fas fa-wallet me-2"></i> Wallet</div>
       <div class="menu-item"><i class="fas fa-plus-circle me-2 text-success"></i> Deposit/Withdrawal</div>
-      <div class="menu-item"><i class="fas fa-user me-2"></i> Profile</div>
+      <div class="menu-item"><i class="fas fa-balance-scale me-2"></i> Profit/Loss</div>
       <div class="menu-item"><i class="fas fa-star me-2"></i> Favourite</div>
       <div class="menu-item"><i class="fas fa-user-friends me-2"></i> Refer And Earn</div>
       <div class="menu-item"><i class="fas fa-file-alt me-2"></i> Account Statement</div>
       <div class="menu-item"><i class="fas fa-tasks me-2"></i> Unsettled Bets</div>
-      <div class="menu-item"><i class="fas fa-balance-scale me-2"></i> Profit/Loss</div>
       <div class="menu-item"><i class="fas fa-chart-pie me-2"></i> Market Analysis</div>
       <div class="menu-item"><i class="fas fa-key me-2"></i> Change Password</div>
       <div class="menu-item text-danger"><i class="fas fa-sign-out-alt me-2"></i> Logout</div>
