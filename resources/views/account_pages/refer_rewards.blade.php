139d998237d7cde77f19b0db085f4057421e04c2
juhi awesome007 <juhiawesome03@gmail.com>
2025-09-02 17:22:43 +0530

    remove bordar-outline from refer_rewards

--- resources/views/account_pages/refer_rewards.blade.php
+++ resources/views/account_pages/refer_rewards.blade.php
@@ -49,7 +49,7 @@
         <div class="mb-3">
             <label for="referralLink" class="form-label">Refer Link</label>
             <div class="input-group">
-                <input type="text" class="form-control" id="referralLink" placeholder="Enter Referral Link" readonly>
+                <input type="text" class="form-control" id="referralLink" placeholder="Enter Referral Link"disabled readonly>
                 <button class="btn btn-dark" type="button">Copy</button>
             </div>
         </div>
