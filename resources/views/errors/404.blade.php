87d0d700404f7e9a46261612f84dc98244deeb96
prateekawesom04-cloud <prateek.awesom.04@gmail.com>
2025-09-11 13:54:07 -0700

    404 page updates

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,7 +2,7 @@
 
 @section('body')
 
-<div class="flex items-center justify-center">
+<div class="flex w-[100vw] h-[100vh] items-center justify-center absolute left-0 top-0 fs-2 font-bold">
     Page Not Found
 </div>
 
