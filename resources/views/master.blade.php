80cd9404aeab8658cead3ba3e5ad156e60e65a3a
prateekawesom04-cloud <prateek.awesom.04@gmail.com>
2025-09-10 17:39:45 -0700

    ezugi, loader update

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -3,17 +3,28 @@
 @include('head')
 
 <body>
+    <div class="app_body" style="display:none;">
     @include('header')
     @include('account-sidebar')
     @include('navbar')
     @yield('body')
     @include('footer')
+    </div>
     @include('js')
-    <div class="loader" style="display: none !important;">
-        Loading...
+    <div class="app_loader bg-white flex items-center justify-center w-full h-[100vh] absolute left-0 top-0 bg-gray-300 z-[1111]">
+        <img src="{{ asset('images/Hourglass.gif') }}" alt="Logo" height="40" width="100" />
     </div>
 
     @yield('js')
+    
+    <script>
+        $(document).ready(function(){
+            setTimeout(() => {
+                $('.app_body').show();
+                $('.app_loader').fadeOut(500);
+            }, 2000);
+        })
+    </script>
 </body>
 
 </html>
