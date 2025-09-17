7a1204400f84004855c5407aab46aeccd0ec6022
prateekawesom04-cloud <prateek.awesom.04@gmail.com>
2025-09-16 18:50:56 -0700

    design updates

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,5 +1,5 @@
 <!-- Navbar -->
-<div class="bg-primary">
+<div class="">
     <nav class="nav nav-pills flex-nowrap overflow-auto px-1 py-1 small"
         style="white-space: nowrap; scrollbar-width: thin;">
         <a class="nav-link text-white py-1 px-2" href="home">Home</a>
